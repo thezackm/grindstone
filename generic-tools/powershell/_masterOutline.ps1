1b53ea10fe1cb788a4166e0ac0485ef81ce5aa31
thezackm <zack.mutchler@gmail.com>
2019-03-01 05:17:44 +0000

    Initial script creation

---
+++ generic-tools/powershell/_masterOutline.ps1
@@ -0,0 +1,70 @@
+#region Top of Script
+
+#requires -version 3
+<#
+
+.SYNOPSIS 
+    String
+
+.DESCRIPTION 
+    String
+
+.NOTES
+    Version:        1.0
+    Author:         Zack Mutchler
+    Creation Date:  March 1, 2019
+    Purpose/Change: Initial Script development
+  
+#>
+
+#endregion
+
+#####-----------------------------------------------------------------------------------------#####
+
+#region Script Parameters
+
+
+
+#endregion Script Parameters
+
+#####-----------------------------------------------------------------------------------------#####
+
+#region Pre-Work
+
+# Start the transcript
+$logFile = ''
+Start-Transcript -Path $logFile
+
+# Start the stopwatch
+
+
+#endregion Pre-Work
+
+#####-----------------------------------------------------------------------------------------#####
+
+#region Variables
+
+
+
+#endregion Variables
+
+#####-----------------------------------------------------------------------------------------#####
+
+#region Execution
+
+
+
+#endregion Execution
+
+#####-----------------------------------------------------------------------------------------#####
+
+#region Cleanup
+
+# Stop the stopwatch and show results
+
+
+
+# Stop the trainscript
+Stop-Transcript 
+
+#endregion Cleanup
